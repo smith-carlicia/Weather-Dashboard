05b6bbddd6a4e513e827c81ec487ae801b9877c7
Carlicia Smith <carliciasmith@Carlicias-MacBook-Pro-2.local>
2020-10-12 10:13:37 -0400

    updated current weather framework to local file

--- script.js
+++ script.js
@@ -1,17 +1,72 @@
-// Search bar function 
-function searchBar(){
-    var userInput = document.querySelector("#search-input");
-    var cities = document.querySelector.apply("#city-list");
+$(document).ready(function(){
+    $(function() {
+
+        var userInput = document.querySelector("#search-input");
+        var forecast = document.querySelector("#five-day-forecast");
+        var searchbutton = document.querySelector("#searchbtn");
+        var searchHistory = doument.querySelector("#search-engine");
     
-    var search = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=649856b9ade664fe56ba1ab8c053cf86"
+        moment().format("L");
     
-    $ajax({
-        url: search,
-        method:"GET"
-    }).then(function(response){
-        render userSearch() {
-            var cityList = $("<li>");
-            
-        }
-    });
-};
+        function searchBar(){
+            // var cities = document.querySelector.apply("#city-list");
+            var cityName = userInput.value;
+    
+            var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=649856b9ade664fe56ba1ab8c053cf86"
+            var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=649856b9ade664fe56ba1ab8c053cf86"
+            console.log(currentWeatherURL);
+            var currentDay = moment().format();
+            $(".cityandcurrentdate").text(moment().format("L"));
+    
+    
+            $.ajax({
+                url: currentWeatherURL,
+                method:"GET"
+            }).then(function(response){
+                console.log(response.main.temp);
+                $(".cityandcurrentdate").text(response.name + response.date + response.icon);
+                $(".tempF").text("Temperature: " + response.main.temp);
+                $(".humidity").text("Humidity: " + response.main.humidity);
+                $(".wind").text("Wind Speed: " + response.wind.speed);
+                $("uv").text("UV Index: " + response.uv.index);
+
+                var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+                $(".temp").text("Temperature (K) " + response.main.temp);
+                $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+            });
+    // Render search 
+           renderSearch();
+            function renderSearch(){
+                userInput.innerHTML = "";
+                for (var i = 0; i < search.length; i++){
+                    var search = search[i];
+                    var li = document.createElement("li");
+                    li.textContent = search;
+                    cities.appendChild(li);
+                }
+            }
+    // Search button click event
+            searchbutton.addEventListener("click", function(event){
+                event.preventDefault();
+                searchBar();
+            });
+    // Search submission 
+            searchHistory.addEventListener("submit", function(event){
+                event.preventDefault();
+    
+                var searchText = userInput.value.trim();
+                if (searchText === "") {
+                    return;
+                  }
+    
+                cityName.push(searchText);
+                userInput.value = "";
+    
+                renderSearch();
+            })
+    
+        };
+        // Five day forecast card forms
+    
+    }); //end ready
+});
