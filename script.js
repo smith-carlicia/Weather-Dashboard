1ca3134aa838e39821f0d390ef25f23165ba7220
Carlicia Smith <carliciasmith@Carlicias-MacBook-Pro-2.local>
2020-10-12 16:00:08 -0400

    updated current weather date via moment.js to local file

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-$(document).ready(function(){
-    $(function(){
+$(function(){
 
-        var moment = moment().isDate(newDate());
+        // var currentDate = moment().isDate(newDate());
         var userInput = document.querySelector("#search-input");
         var forecast = document.querySelector("#five-day-forecast");
         var searchbutton = document.querySelector("#searchbtn");
         var searchHistory = document.querySelector("#search-engine");
     
         
-    
+        searchBar();
         function searchBar(){
             // var cities = document.querySelector.apply("#city-list");
-            var cityName = userInput.value;
+            var currentCityName = userInput.value;
     
-            var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=649856b9ade664fe56ba1ab8c053cf86"
-            var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=649856b9ade664fe56ba1ab8c053cf86"
+            var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + 
+            currentCityName + "&appid=649856b9ade664fe56ba1ab8c053cf86"
+
+            var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + 
+            currentCityName + "&appid=649856b9ade664fe56ba1ab8c053cf86"
             console.log(currentWeatherURL);
             // var currentDay = moment().format();
             $(".cityandcurrentdate").text(moment().format("L"));
@@ -24,50 +26,61 @@ $(document).ready(function(){
                 url: currentWeatherURL,
                 method:"GET"
             }).then(function(response){
-                console.log(response.main.temp);
                 $(".cityandcurrentdate").text(response.name + response.date + response.icon);
+                console.log(response.name + response.date + response.icon);
                 $(".tempF").text("Temperature: " + response.main.temp);
+                console.log("Temperature" + response.main.temp);
                 $(".humidity").text("Humidity: " + response.main.humidity);
+                console.log("Humidity: " + response.main.humidity);
                 $(".wind").text("Wind Speed: " + response.wind.speed);
+                console.log("Wind Speed: " + response.wind.speed);
                 $("uv").text("UV Index: " + response.uv.index);
+                console.log("UV Index: " + response.uv.index);
 
                 var tempF = (response.main.temp - 273.15) * 1.80 + 32;
                 $(".temp").text("Temperature (K) " + response.main.temp);
                 $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
             });
-    // Render search 
+            // Render search 
            renderSearch();
-            function renderSearch(){
-                userInput.innerHTML = "";
-                for (var i = 0; i < search.length; i++){
-                    var search = search[i];
-                    var li = document.createElement("li");
-                    li.textContent = search;
-                    cities.appendChild(li);
-                }
-            }
-    // Search button click event
-            searchbutton.addEventListener("click", function(event){
-                event.preventDefault();
-                searchBar();
-            });
-    // Search submission 
-            searchHistory.addEventListener("submit", function(event){
-                event.preventDefault();
-    
-                var searchText = userInput.value.trim();
-                if (searchText === "") {
-                    return;
-                  }
-    
-                cityName.push(searchText);
-                userInput.value = "";
-    
-                renderSearch();
-            })
+           function renderSearch(){
+               userInput.innerHTML = "";
+               for (var i = 0; i < search.length; i++){
+                   var search = search[i];
+                   var li = document.createElement("li");
+                   li.textContent = search;
+                   cities.appendChild(li);
+               }
+           }
+   // Search button click event
+           searchbutton.addEventListener("click", function(event){
+               event.preventDefault();
+               searchBar();
+           });
+   // Search submission 
+           searchHistory.addEventListener("submit", function(event){
+               event.preventDefault();
+   
+               var searchText = userInput.value.trim();
+               if (searchText === "") {
+                   return;
+                 }
+   
+               cityName.push(searchText);
+               userInput.value = "";
+   
+               renderSearch();
+           })
     
         };
         // Five day forecast card forms
+        // function cards(){
+        //     $.ajax({
+        //         url: forecastURL,
+        //         method: "GET"
+        //     }).then(response){
+
+        //     }
+        // };
     
-    }); //end ready
-});
+}); //end ready
