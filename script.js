0701d01467c8b3fe6f9b4da138ccb4bd659c9921
Carlicia Smith <carliciasmith@Carlicias-MacBook-Pro-2.local>
2020-10-13 12:50:48 -0400

    updated debugged javascript to local file

--- script.js
+++ script.js
@@ -1,55 +1,74 @@
 $(function(){
 
-        // var currentDate = moment().isDate(newDate());
-        var userInput = document.querySelector("#search-input");
-        var forecast = document.querySelector("#five-day-forecast");
-        var searchbutton = document.querySelector("#searchbtn");
-        var searchHistory = document.querySelector("#search-engine");
-    
+    // var currentDate = moment().isDate(newDate());
         
         searchBar();
         function searchBar(){
-            // var cities = document.querySelector.apply("#city-list");
-            var currentCityName = userInput.value;
+            var searchbutton = document.querySelector("#searchbtn");
+            var searchInput = document.querySelector("#search-input").value;
+            var searchHistory = document.querySelector("#search-engine");
+
+            var currentCityName = "";
+            // var userInput = $("#search-input").value;
+            console.log("#search-input");
     
             var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + 
             currentCityName + "&appid=649856b9ade664fe56ba1ab8c053cf86"
+            console.log(forecastURL);
+            
 
             var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + 
             currentCityName + "&appid=649856b9ade664fe56ba1ab8c053cf86"
             console.log(currentWeatherURL);
+            
             // var currentDay = moment().format();
             $(".cityandcurrentdate").text(moment().format("L"));
     
-    
+
             $.ajax({
                 url: currentWeatherURL,
                 method:"GET"
             }).then(function(response){
-                $(".cityandcurrentdate").text(response.name + response.date + response.icon);
+                console.log(response);
+                console.log(currentWeatherURL);
+                // var todayWeather = response.data;
+                var currentWeatherCard = $(".currentweather");
+
+                var currentWeather = $(".cityandcurrentdate").text(response.name + response.date + response.icon);
                 console.log(response.name + response.date + response.icon);
-                $(".tempF").text("Temperature: " + response.main.temp);
+                // console.log("testing");
+                var temperature = $(".tempF").text("Temperature: " + response.main.temp);
                 console.log("Temperature" + response.main.temp);
-                $(".humidity").text("Humidity: " + response.main.humidity);
+
+                var humidity = $(".humidity").text("Humidity: " + response.main.humidity);
                 console.log("Humidity: " + response.main.humidity);
-                $(".wind").text("Wind Speed: " + response.wind.speed);
+
+                var wind = $(".wind").text("Wind Speed: " + response.wind.speed);
                 console.log("Wind Speed: " + response.wind.speed);
-                $("uv").text("UV Index: " + response.uv.index);
+
+                var uv = $("uv").text("UV Index: " + response.uv.index);
                 console.log("UV Index: " + response.uv.index);
 
-                var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-                $(".temp").text("Temperature (K) " + response.main.temp);
-                $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+                currentWeatherCard.append(currentWeather, temperature, humidity, wind, uv);
+                $("body").append(currentWeatherCard);
+
+                // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+                // $(".temp").text("Temperature (K) " + response.main.temp);
+                // $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
             });
             // Render search 
            renderSearch();
            function renderSearch(){
-               userInput.innerHTML = "";
-               for (var i = 0; i < search.length; i++){
-                   var search = search[i];
+            var cities = document.querySelector("#city-list");
+            console.log("#city-list");
+               searchInput.innerHTML = "";
+               for (var i = 0; i < cities.length; i++){
+                   var cities = cities[i];
+                   
                    var li = document.createElement("li");
-                   li.textContent = search;
+                   li.textContent = cities;
                    cities.appendChild(li);
+                   $()
                }
            }
    // Search button click event
@@ -57,30 +76,41 @@ $(function(){
                event.preventDefault();
                searchBar();
            });
-   // Search submission 
+   // Submission 
            searchHistory.addEventListener("submit", function(event){
                event.preventDefault();
    
-               var searchText = userInput.value.trim();
+               var searchText = searchInput.value.trim();
                if (searchText === "") {
                    return;
                  }
    
                cityName.push(searchText);
-               userInput.value = "";
-   
+               searchInput.value = "";
+                 
                renderSearch();
            })
     
         };
         // Five day forecast card forms
         // function cards(){
+            
         //     $.ajax({
         //         url: forecastURL,
         //         method: "GET"
         //     }).then(response){
-
+                // var forecast = document.querySelector("#five-day-forecast");
+        //         $("#card-date").text(response.date);
+        //         $("#card-icon").text(response.icon);
+        //         $("#card-temperature").text(responce.main.temperature);
+        //         $("#card-humidity").text(response.humidity);
         //     }
         // };
-    
+        //     var forecastCards = ["card-one", "card-two", "card-three", "card-4", "card-5"];
+
+        //     for (var i = 0; i < forecastCards.length; i++){
+                   
+        //     }
+           
+        
 }); //end ready
